dae04d6962ee9cda7db594c52da54f83f4de4c7c
anasmansouri <htmlphpcss3@gmail.com>
2020-03-17 15:28:20 +0100

    i want to import the Driver Class

---
+++ Hard/anas/driver/ClassDriverGSEII/MyPin.py
@@ -0,0 +1,50 @@
+
+class MyPin:
+
+    def __init__(self, GpioNumber ,direction ,value):
+        self._GpioNumber = GpioNumber
+        self.value = value
+        self.direction = direction
+
+    # direction
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        self._direction = direction
+        d = open("/sys/class/gpio/gpio{}/direction".format(self._GpioNumber), "w")
+        d.write("{}".format(self._direction))
+        d.close()
+
+    # value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+        v = open("/sys/class/gpio/gpio{}/value".format(self._GpioNumber), "w")
+        v.write("{}".format(self._value))
+        v.close()
+
+    # export gpio
+    def export(self):
+        f = open("/sys/class/gpio/export", "w")
+        f.write("{}".format(self._GpioNumber))
+        f.close()
+
+    # enable pin - diretion - status
+
+    def __repr__(self):
+        return "MyPin(GpioNumber : {} ,direction : {} ,value : {})".format(self._GpioNumber,
+                                                                              self.value,
+                                                                              self.direction,
+                                                                              )
+
+    @staticmethod
+    def description():
+        return "this is a GpioDriver"
