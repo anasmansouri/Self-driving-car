3d24167aa1c578cc0e6a37de7f62d06e599de33c
anasmansouri <htmlphpcss3@gmail.com>
2020-03-17 15:18:50 +0100

    i finished the driver class

--- Hard/anas/driver/gpioDriver.py
+++ Hard/anas/driver/gpioDriver.py
@@ -1,4 +1,12 @@
 
-f = open("/sys/class/gpio/export")
-f.write("18")
-f.close()
+#f = open("/sys/class/gpio/export","w")
+#f.write("18")
+#f.close()
+
+#d = open("/sys/class/gpio/gpio18/direction","w")
+#d.write("out")
+#d.close()
+
+v = open("/sys/class/gpio/gpio18/value","w")
+v.write("0")
+v.close()
